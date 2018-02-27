7a725f58d1e98d866c689fcbb13e26ce1b104f21
gomesalexandre <alexandrego2003@gmail.com>
2018-02-28 00:15:31 +0100

    Added test for setter

--- InboxProject/test/Inbox.test.js
+++ InboxProject/test/Inbox.test.js
@@ -9,9 +9,10 @@ const { interface, bytecode } = require('../compile');
 const defaultMessage = "Hey there";
 
 let inbox;
+let accounts;
 
 beforeEach(async () => {
-  let accounts = await web3.eth.getAccounts();
+  accounts = await web3.eth.getAccounts();
 
   // Contract deployment
   inbox = await new web3.eth.Contract(JSON.parse(interface))
@@ -34,5 +35,15 @@ describe('Inbox', () => {
   it('Constructor functions sets default message', async () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, defaultMessage);
+  });
+  it('Message setter is working', async () => {
+    await inbox.methods.setMessage('Bon appétit !').send(
+      {
+        from: accounts[0],
+        gas: '1000000'
+      }
+    )
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Bon appétit !');
   });
 });
