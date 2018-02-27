5f3c88a720ca1fa40714a0fb338f6e01d9a3d265
gomesalexandre <alexandrego2003@gmail.com>
2018-02-28 00:10:54 +0100

    Added test to check default message is set

--- InboxProject/test/Inbox.test.js
+++ InboxProject/test/Inbox.test.js
@@ -1,29 +1,38 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
+const provider = ganache.provider();
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
-let accounts;
+const defaultMessage = "Hey there";
+
 let inbox;
 
 beforeEach(async () => {
-  accounts = await web3.eth.getAccounts();
+  let accounts = await web3.eth.getAccounts();
 
   // Contract deployment
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ['Hey there']
+      arguments: [defaultMessage]
     })
     .send({
       from: accounts[0],
       gas: '1000000'
     });
+
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
-  it('Should get Ganache testRPC accounts', () => {
-    console.log(inbox); // Deployed contract
+  it('Deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+  it('Constructor functions sets default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, defaultMessage);
   });
 });
