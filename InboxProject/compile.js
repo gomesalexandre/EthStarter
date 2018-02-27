2b96df82dbda952348542c3838c3a944b01b822d
gomesalexandre <alexandrego2003@gmail.com>
2018-02-27 23:25:08 +0100

    Added deployment test

--- InboxProject/compile.js
+++ InboxProject/compile.js
@@ -5,5 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // Cross-Compatibility path-building
 const contractSource = fs.readFileSync(inboxPath, 'utf8');
 
-solc.compile(contractSource, 1).contracts[':Inbox']; // Compiling to ABI + Bytecode
-
+module.exports = solc.compile(contractSource, 1).contracts[':Inbox']; // Compiling to ABI + Bytecode
--- InboxProject/test/Inbox.test.js
+++ InboxProject/test/Inbox.test.js
@@ -2,3 +2,28 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  // Contract deployment
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hey there']
+    })
+    .send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+});
+
+describe('Inbox', () => {
+  it('Should get Ganache testRPC accounts', () => {
+    console.log(inbox); // Deployed contract
+  });
+});
