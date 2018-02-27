02bc3270c9464aac59c952993ed2bdd3887fd4a6
gomesalexandre <alexandrego2003@gmail.com>
2018-02-27 22:37:03 +0100

    Updated packages, added mocha to package json

--- InboxProject/compile.js
+++ InboxProject/compile.js
@@ -5,5 +5,5 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // Cross-Compatibility path-building
 const contractSource = fs.readFileSync(inboxPath, 'utf8');
 
-solc.compile(contractSource, 1); // Compiling to ABI + Bytecode
+solc.compile(contractSource, 1).contracts[':Inbox']; // Compiling to ABI + Bytecode
 
