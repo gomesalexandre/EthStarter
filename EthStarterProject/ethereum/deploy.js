4d370e83d07e84121d73d166b12c3c948b5fc223
gomesalexandre <alexandrego2003@gmail.com>
2018-03-07 20:11:13 +0100

    Changed folder structure, added next

--- EthStarterProject/ethereum/deploy.js
+++ EthStarterProject/ethereum/deploy.js
@@ -1,3 +1,5 @@
+const util = require('util');
+const fs = require('fs');
 const WalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
@@ -6,6 +8,8 @@ const config = require('./config'); // Mneumonics are safe in a gitignored confi
 const provider = new WalletProvider(config.mneumo, config.endpoint);
 const web3 = new Web3(provider);
 
+fs.writeFile = util.promisify(fs.writeFile); // Making us able to use async/await on fs.writeFile
+
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log('Deploying from account', accounts[0]);
@@ -13,6 +17,7 @@ const deploy = async () => {
     .deploy({ data: compiledFactory.bytecode})
     .send({gas: '1000000', from: accounts[0]});
 
+  await fs.writeFile('./ethereum/factoryAddress.json', JSON.stringify({address: deployedContract.options.address}), 'utf-8');
   console.log('Contract deployed to', deployedContract.options.address);
 };
 deploy();
---
+++ EthStarterProject/pages/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => (<h1>INDEX page</h1>);
