a3bba0783f56463f107a13a05ffb3ca20fc4195c
gomesalexandre <alexandrego2003@gmail.com>
2018-03-21 15:17:52 +0100

    Card component creator now in its own file

--- EthStarterProject/pages/campaign/campaign.js
+++ EthStarterProject/pages/campaign/campaign.js
@@ -10,8 +10,10 @@ class Campaign extends React.Component {
   constructor(props) {
     super(props);
   }
+
   state = {
-    isModalVisible : false, isRequestModalVisible: false,
+    isRequestModalVisible: false,
+    isContributeCardVisible: false,
     requestDescription: '',
     requestRecipient: '',
     requestValue: 0,
@@ -34,6 +36,9 @@ class Campaign extends React.Component {
   showRequestForm() {
     this.setState({isRequestCardVisible: true,});
   }
+  showContributeForm() {
+    this.setState({isContributeCardVisible: true, });
+  }
   showRequestModal(data) {
     this.setState({isRequestModalVisible: true,});
 
@@ -47,7 +52,7 @@ class Campaign extends React.Component {
       });
     } catch(e) {throw e;}
   }
-  async handleOk() {
+  async handleRequestOk() {
     this.setState({loading: true,});
 
     try {
@@ -79,7 +84,7 @@ class Campaign extends React.Component {
         <Modal
         visible={this.state.isRequestModalVisible}
         title="New Request"
-        onOk={() => this.handleOk()}
+        onOk={() => this.handleRequestOk()}
         okText={this.state.loading ? <Icon type="loading" /> : 'Request'}
         >
          <List
@@ -128,7 +133,7 @@ class Campaign extends React.Component {
               <Layout.Content>
                 <Card title="Campaign">
                   <Button type={this.state.isRequestCardVisible ? "primary" : "secondary"} icon="file" onClick={() => this.showRequestForm()}>Create new request</Button>
-                  <Button type="secondary" icon="file">Contribute</Button>
+                  <Button type="secondary" icon="file" onClick={() => this.showContributeForm()}>Contribute</Button>
                   { this.state.isRequestCardVisible &&
                     <Card type="inner" title="New request">
                       <Form layout="inline">
@@ -142,13 +147,13 @@ class Campaign extends React.Component {
                         {getFieldDecorator('requestValue', {
                             rules: [{ required: true, whitespace: true, },],
                             })(<Input />)
-                          }
+                        }
                         </Form.Item>
                         <Form.Item label= "Recipient">
                         {getFieldDecorator('requestRecipient', {
                             rules: [{ required: true, whitespace: true, },],
                             })(<Input />)
-                          }
+                        }
                         </Form.Item>
                         <Button type="primary" onClick={() => this.showRequestModal()}>Request</Button>
                       </Form>
@@ -167,5 +172,5 @@ class Campaign extends React.Component {
       </PageLayout>
     );}
 }
-const CampaignParsed = Form.create({})(Campaign);
-export default CampaignParsed;
+const ParsedCampaign = Form.create({})(Campaign);
+export default ParsedCampaign;
--- EthStarterProject/pages/index.js
+++ EthStarterProject/pages/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import Link from 'next/link';
 import { Layout, Menu, Breadcrumb, Icon, Card, Row, Col, Button, Tag, List, } from 'antd';
+import web3 from '../ethereum/web3';
 
 import factory from '../ethereum/factory';
 import PageLayout from '../components/Layout';
 
+import CampaignCard from '../components/CampaignCard';
 const { Header, Content, } = Layout;
 
-const campaignCard = (address, key) => {
-  return(
-    <Col key={key}>
-      <Card
-        title={address}
-        extra={
-          <Link href={`/campaign?id=${address}`} as={`/campaign/${address}`} prefetch>
-            More
-          </Link>}
-        style={{ width: "500px", }}>
-        <p><a href={`https://rinkeby.etherscan.io/address/${address}`}>View on EtherScan</a></p>
-      </Card>
-    </Col>);
-};
+
 
 class CampaignsIndex extends React.Component {
   static async getInitialProps() {
@@ -29,11 +17,12 @@ class CampaignsIndex extends React.Component {
   }
 
   render() {
+    console.log('wbeb3 is', web3.currentProvider);
     return(
       <PageLayout selected="campaigns">
         <h1 style={{"textAlign" : "center",}}>Opened campaigns</h1>
         <Row type="flex" justify="center">
-          {this.props.campaigns.map( (campaignAddress, i) => campaignCard(campaignAddress, i))}
+          {this.props.campaigns.map( (campaignAddress, i) => CampaignCard(campaignAddress, i))}
         </Row>
         <Row type="flex" justify="left">
           <Button type="primary">Create a campaign</Button>
